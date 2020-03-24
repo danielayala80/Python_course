58465f2dae8e21af64a8c96e6f918734476c3c5c
danielayala80 <42308183+danielayala80@users.noreply.github.com>
2020-03-24 11:51:42 -0700

    helloWorld
    Simple helloWorld

---
+++ helloWorld/main.py
@@ -0,0 +1,14 @@
+#pirnt hello world
+global x 
+x = "awesome"
+
+def myFunc():
+    x = 'hello world'
+    print("X is: " + x)
+
+def Main():
+    myFunc()
+
+if __name__=="__main__":
+    Main()
+    
